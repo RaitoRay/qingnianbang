9be53ab98f920eb7fb767500271f9903c214450c
guolingjiang <1183567763@qq.com>
2023-09-24 18:17:04 +0800

    测试

---
+++ pages/index/index.vue
@@ -0,0 +1,66 @@
+<template>
+	<view class="home">
+		<scroll-view scroll-x class="navscroll">
+			<view class="item" v-for="item in 10">国内</view>
+		</scroll-view>
+		<view class="content">
+			<view class="row" v-for="itam in 10">
+				<newsbox></newsbox>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.navscroll{
+	height: 100rpx;
+	background: #F7F8FA;
+	white-space: nowrap;
+	position: fixed;
+	top:var(--window-top);
+	left:0;
+	z-index: 10;
+	/deep/ ::-webkit-scrollbar {
+		width: 4px !important;
+		height: 1px !important;
+		overflow: auto !important;
+		background: transparent !important;
+		-webkit-appearance: auto !important;
+		display: block;
+	}
+	.item{
+		font-size: 40rpx;
+		display: inline-block;
+		line-height: 100rpx;
+		padding:0 30rpx;
+		color:#333;		
+		&.active{
+			color:#31C27C;
+		}
+	}
+}
+.content{
+	padding:30rpx;
+	padding-top:130rpx;	
+	.row{
+		border-bottom:1px dotted #efefef;
+		padding:20rpx 0;
+	}
+}
+</style>
---
+++ pages/user/user.vue
@@ -0,0 +1,68 @@
+<template>
+	<view class="user">
+		<view class="top">
+			<image src="../../static/images/history.png" mode=""></image>
+			<view class="text">浏览历史</view>
+		</view>
+		<view class="content">
+			<view class="row" v-for="item in 10">
+				<newsbox></newsbox>
+			</view>			
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+.user{
+	.top{
+		padding:50rpx 0;
+		background: #F8F8F8;
+		color:#666;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		image{
+			width: 150rpx;
+			height: 150rpx;
+		}
+		.text{
+			font-size: 38rpx;		
+			padding-top: 20rpx;
+		}
+	}
+	.content{
+		padding:30rpx;
+		.row{
+			border-bottom:1px dotted #efefef;
+			padding:20rpx 0;
+		}
+	}
+	.nohistory{
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		image{
+			width: 450rpx;
+		}
+		.text{
+			font-size: 26rpx;
+			color:#888;
+		}
+	}
+}
+</style>
